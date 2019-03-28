546f3dfe2cc31b7b182fa5020be6dee5ffff2f49
marciomarquessouza <marciomultimedia@gmail.com>
2019-03-28 01:40:01 -0300

    feat: test coverage

--- tests/routes/auth.test.js
+++ tests/routes/auth.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../app');
+const app = require('../../app.js');
 const User = require('../../src/models/user');
 const bcrypt = require('bcryptjs');
 
